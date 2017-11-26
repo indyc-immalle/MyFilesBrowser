15c1de375c2b505eee626940a2b169640b6422c9
Indy Caluwe <indy.caluwe@immalle.be>
2017-11-26 21:29:52 +0100

    2 methodes zijn toegevoed

--- MYfileBrowseR/Program.cs
+++ MYfileBrowseR/Program.cs
@@ -2,14 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace MYfileBrowseR
+
+namespace Myfilebrowser
 {
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("geef een argument" +
+                    "-h = help");
+            }
+            else
+            {
+                switch (args[0])
+                {
+                    case "-f":
+                        ShowAllFilesInCurrentDirectory();
+                        break;
+
+                    case "-h":
+                        Console.WriteLine("-c = huidige directory");
+                        Console.WriteLine("-f = alle files huidige directory, ");
+                        Console.WriteLine("-d = alle subdirectories in de huidige directory");
+                        break;
+
+                    case "-d":
+                        AllSubDirectorysInCurrentDirectory();
+                        break;
+
+
+                    case "-c":
+                        CurrentDirectory();
+                        break;
+
+                    default:
+                        Console.WriteLine("onbekend argument");
+                        break;
+
+                }
+
+            }
         }
+
+        private static void CurrentDirectory()
+        {
+            Console.WriteLine("directory " + Environment.CurrentDirectory);
+
+
+
+        }
+
+        private static void ShowAllFilesInCurrentDirectory()
+        {
+            Console.WriteLine("alle files in huidige directory");
+
+            string[] dirs = Directory.GetFiles(Environment.CurrentDirectory);
+            foreach (string dir in dirs)
+            {
+                Console.WriteLine(Path.GetFileName(dir));
+            }
+        }
+
+        
     }
 }
+
